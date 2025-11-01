6e6c655d6d99218ab8b979f621c2cb949834e06f
Jamming17 <jackchiplin@outlook.com>
2025-11-01 11:54:00 +0000

    Removed dotenc and nodemon as node now provides that functionality

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 import authRoutes from "./routes/proxy.js"
-
-dotenv.config();
 
 const app = express();
 app.use(cors());
